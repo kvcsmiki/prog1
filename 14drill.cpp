54b9c55a6e7c954933cc7bd3a72f35266ace018d
kvcsmiki <55454487+kvcsmiki@users.noreply.github.com>
2021-03-16 11:04:42 +0100

    hehej 14drill

---
+++ 14drill.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+class B1 {
+public:
+	virtual void vf() const
+			{ std::cout << "B1::vf\n";}
+	void f() const { std::cout << "B1::f\n"; }
+	virtual void pvf() = 0;
+};
+
+class D1 : public B1 {
+public:	
+	void vf() const override 
+		{ std::cout << "D1::vf\n";}
+	void f() const
+		{ std::cout << "D1::f\n";}
+};
+
+class D2 : public D1{
+public:
+	void pvf() override
+		{std::cout << "D2::pvf\n";}
+};
+
+class B2{
+public:
+	virtual void pvf() = 0;
+};
+
+class D21 : public B2{
+public:
+	int member;
+	void pvf() override {std::cout << "D21::pvf\n" << member << "\n";}
+};
+class D22 : public B2{
+public:
+	int member;
+	void pvf() override {std::cout << "D22::pvf\n" << member << "\n";}
+};
+
+void f(B2& b){
+	b.pvf();
+}
+
+
+int main()
+{
+	/*B1 b;
+	b.vf();
+	b.f();
+
+	D1 d;
+	d.vf();
+	d.f();
+
+	B1& bref{d};
+	bref.vf();
+	bref.f();*/
+
+	D2 d2;
+	d2.vf();
+	d2.f();
+	d2.pvf();
+
+	D21 d21;
+	D22 d22;
+	f(d21);
+	f(d22);
+	return 0;
+}
