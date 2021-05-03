52d966e64257b5c90963725bd9a24a371cbcaf7a
kvcsmiki <55454487+kvcsmiki@users.noreply.github.com>
2021-05-03 13:57:39 +0200

    better
    final

--- drill21/drill21.cpp
+++ drill21/drill21.cpp
@@ -149,11 +149,11 @@ int main()
     //MAP
     std::map<std::string,int> msi;
 
-	std::string pair1 = "pair1";
+	std::string pair1 = "pair0";
 	for(int i = 0; i < 10; ++i)
 	{
 		msi.insert(std::pair<std::string,int>(pair1, i + 1 ));
-		pair1 = pair1.substr(0,4) + std::to_string(i+2);
+		pair1 = pair1.substr(0,4) + std::to_string(i+1);
 	}
 
 	my_print(msi);
